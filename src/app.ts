4fee46c5545900cc6edb6adf3abac7bda46e586d
Jv1ctor <j.rodriguesbarretos@gmail.com>
2024-01-19 18:11:35 -0300

    initial project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import "dotenv/config"
+import morgan from "morgan"
+import helmet from "helmet"
+
+const app = express()
+const port = process.env.PORT || 3333
+
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+
+app.listen(port, () => {
+  console.log("server listening...")
+})
